b81184514ddcd433480a1ed98e89cd536eb45a6f
muhammadmehdi09 <120133578+muhammadmehdi09@users.noreply.github.com>
2024-01-15 12:50:10 +0500

    Update sanietyClient.ts

--- src/lib/sanietyClient.ts
+++ src/lib/sanietyClient.ts
@@ -4,8 +4,8 @@ import { createClient } from 'next-sanity'
 
 export const client = createClient({
     apiVersion: "2023-08-04",
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    token: process.env.NEXT_PUBLIC_SANITY_PROJECT_API_TOKEN,
+    dataset: "production",
+    projectId: "6vj92eqt",
+    token: "skPq98fptRxYCg03pBma7l9tHApNblg5VfoyZssNRfxXFDcspV6MakOLu070dioZ2SJyWvvRmplfM0FrqndrHSBnfoqOuaiUqG6RBRNYetgiLJYZaDVY4FxNLYZ6my7BCKMOyi5V7Euu9jkGwQJXOzq2NPmxy6fY86Ift4yGxlUMbnoiuL86",
     useCdn: true,
 })
